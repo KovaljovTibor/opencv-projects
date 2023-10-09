e3d0dfa4dc036fe8dbe0c0e44625daa029797647
kovaljovtibor <tibor.k@numfil.com>
2023-10-09 19:17:48 +0200

    added Camera Calibration

--- CameraCalibration/main.cpp
+++ CameraCalibration/main.cpp
@@ -1,10 +1,10 @@
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/aruco.hpp>
 #include <opencv2/calib3d.hpp>
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
 
@@ -14,7 +14,7 @@ const float arucoSquareDimension = 0.26800f;
 const cv::Size chessboardDimensions = cv::Size(6, 9);
 
 
-void CreateArucoMarkers() {
+void createArucoMarkers() {
     cv::Mat outputMarker;
     cv::Ptr<cv::aruco::Dictionary> markerDictionary =
             cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
@@ -29,7 +29,7 @@ void CreateArucoMarkers() {
 }
 
 
-void CreateKnownBoardPosition(cv::Size& boardSize, float squareEdgeLength, std::vector<cv::Point3f>& points) {
+void createKnownBoardPosition(cv::Size& boardSize, float squareEdgeLength, std::vector<cv::Point3f>& points) {
     for (int i = 0; i < boardSize.height; i++) {
         for (int j = 0; j < boardSize.width; j++) {
             points.emplace_back((float)j * squareEdgeLength, (float)i * squareEdgeLength, 0.0f);
@@ -38,7 +38,7 @@ void CreateKnownBoardPosition(cv::Size& boardSize, float squareEdgeLength, std::
 }
 
 
-void GetIntersections(std::vector<cv::Mat>& images, std::vector<std::vector<cv::Point2f>>& allFoundPoints,
+void getChessboardCorners(std::vector<cv::Mat>& images, std::vector<std::vector<cv::Point2f>>& allFoundPoints,
                           bool showResults = false) {
     for (auto iter = images.begin(); iter != images.end(); iter++) {
         std::vector<cv::Point2f> pointBuffer;
@@ -53,6 +53,57 @@ void GetIntersections(std::vector<cv::Mat>& images, std::vector<std::vector<cv::
             cv::waitKey();
         }
     }
+}
+
+
+void cameraCalibration(std::vector<cv::Mat> calibrationImages, cv::Size boardSize,
+                       float squareEdgeLength, cv::Mat& cameraMatrix, cv::Mat& distanceCoefficients) {
+    std::vector<std::vector<cv::Point2f>> checkerboardImageSpacePoints;
+    getChessboardCorners(calibrationImages, checkerboardImageSpacePoints, false);
+
+    std::vector<std::vector<cv::Point3f>> worldSpaceCornerPoints(1);
+    createKnownBoardPosition(boardSize, squareEdgeLength, worldSpaceCornerPoints[0]);
+
+    worldSpaceCornerPoints.resize(checkerboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+
+    std::vector<cv::Mat> rVectors, tVectors;
+    distanceCoefficients = cv::Mat::zeros(8, 1, CV_64F);
+
+    cv::calibrateCamera(worldSpaceCornerPoints, checkerboardImageSpacePoints, boardSize,
+                        cameraMatrix, distanceCoefficients, rVectors, tVectors);
+}
+
+
+bool saveCameraCalibration(std::string name, cv::Mat cameraMatrix, cv::Mat distanceCoefficients) {
+    std::ofstream outStream(name);
+
+    if (outStream) {
+        uint16_t rows = cameraMatrix.rows;
+        uint16_t columns = cameraMatrix.cols;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                double value = cameraMatrix.at<double>(r, c);
+                outStream << value << '\n';
+            }
+        }
+
+        rows = distanceCoefficients.rows;
+        columns = distanceCoefficients.cols;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                double value = cameraMatrix.at<double>(r, c);
+                outStream << value << '\n';
+            }
+        }
+
+        outStream.close();
+
+        return true;
+    }
+
+    return false;
 }
 
 
@@ -90,6 +141,31 @@ int main() {
             cv::imshow("Webcam", frame);
         }
         char character = cv::waitKey(1000 / fps);
+
+        switch (character) {
+            case ' ':
+                // saving of the image
+                if (found) {
+                    cv::Mat temp;
+                    frame.copyTo(temp);
+                    savedImages.push_back(temp);
+                }
+                break;
+            case 13:    // enter key
+                // start calibration
+                if (savedImages.size() > 15) {
+                    cameraCalibration(savedImages, chessboardDimensions, calibrationSquareDimension,
+                                      cameraMatrix, distanceCoefficients);
+                    saveCameraCalibration("Camera Calibration data", cameraMatrix, distanceCoefficients);
+                }
+                break;
+            case 27:    // escape key
+                // exit program
+                break;
+            default:
+                std::cout << "Error" << '\n';
+                break;
+        }
     }
 
     return 0;
